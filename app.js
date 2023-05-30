5cc882b1ae45b198a06401fd541ac9a6dfbe07ef
RobertOu0213 <starshining12345@gmail.com>
2023-05-30 14:29:48 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const session = require("express-session");
 const app = express();
 const port = 3000;
 const exphbs = require("express-handlebars");
@@ -9,6 +10,14 @@ require("./config/mongoose");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+
+app.use(
+  session({
+    secret: "ThisIsMySecret",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
