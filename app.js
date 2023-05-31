be2906e4557eda3a06c203f47efb3c31c25971a9
RobertOu0213 <starshining12345@gmail.com>
2023-05-31 16:57:17 +0800

    feat: add message partials

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
-const session = require("express-session");
-const app = express();
-const port = 3000;
 const exphbs = require("express-handlebars");
 const methodOverride = require("method-override");
+const flash = require("connect-flash");
+const session = require("express-session");
 const routes = require("./routes");
+const port = 3000;
 const usePassport = require("./config/passport");
+
+const app = express();
 
 require("./config/mongoose");
 
@@ -21,10 +23,13 @@ app.use(
 );
 
 usePassport(app);
+app.use(flash());
 
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated();
   res.locals.user = req.user;
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.warning_msg = req.flash("warning_msg");
   next();
 });
 
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -21,11 +21,27 @@ router.get("/register", (req, res) => {
 
 router.post("/register", (req, res) => {
   const { name, email, password, confirmPassword } = req.body;
+  const errors = [];
+  if (!email || !password || !confirmPassword) {
+    errors.push({ message: "郵件及密碼必填 " });
+  }
+  if (password !== confirmPassword) {
+    errors.push({ message: "密碼與確認密碼不相符" });
+  }
+  if (errors.length) {
+    return res.render("register", {
+      errors,
+      name,
+      password,
+      confirmPassword,
+    });
+  }
   User.findOne({ email })
     .then((user) => {
       if (user) {
-        console.log("users have been registered");
+        errors.push({ message: "此email已註冊過" });
         res.render("register", {
+          errors,
           name,
           email,
           password,
@@ -42,6 +58,7 @@ router.post("/register", (req, res) => {
 
 router.get("/logout", (req, res) => {
   req.logout();
+  req.flash("success_msg", "你已成功登出");
   res.redirect("/users/login");
 });
 
