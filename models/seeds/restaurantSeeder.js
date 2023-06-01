b16893111cbfed9c61c5112d02ca5513b7daef01
RobertOu0213 <starshining12345@gmail.com>
2023-06-02 00:05:01 +0800

    feat: modify seeder

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,6 +1,18 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 const Restaurants = require("../restaurants");
+const User = require("../user");
 const restaurantList = require("./restaurant.json").results;
+
+const SEED_USER1 = {
+  email: "user1@example.com",
+  password: "12345678",
+};
+
+const SEED_USER2 = {
+  email: "user2@example.com",
+  password: "12345678",
+};
 
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
@@ -18,7 +30,52 @@ db.on("error", () => {
 });
 
 db.once("open", () => {
-  console.log("mongodb connected");
-  Restaurants.create(restaurantList).then(() => console.log("run seed done"));
-});
+  bcrypt
+    .genSalt(10)
+    .then((salt) => bcrypt.hash(SEED_USER1.password, salt))
+    .then((hash) =>
+      User.create([
+        {
+          email: SEED_USER1.email,
+          password: hash,
+        },
+        {
+          email: SEED_USER2.email,
+          password: hash,
+        },
+      ])
+    )
+    .then((user) => {
+      const restaurantUser1 = [];
+      for (let i = 0; i < 3; i++) {
+        const userId = user[0]._id;
+        restaurantList[i].userId = userId;
+        restaurantUser1.push(restaurantList[i]);
+      }
 
+      const restaurantUser2 = [];
+      for (let i = 3; i < 6; i++) {
+        const userId = user[1]._id;
+        restaurantList[i].userId = userId;
+        restaurantUser2.push(restaurantList[i]);
+      }
+
+      const restaurant = restaurantUser1.concat(restaurantUser2);
+
+      return Restaurants.create(restaurant);
+    })
+    // .then((user) => {
+    //   console.log(user);
+    //   const restaurant = [];
+    //   for (let i = 3; i < 6; i++) {
+    //     const userId = user[1]._id;
+    //     restaurantList[i].userId = userId;
+    //     restaurant.push(restaurantList[i]);
+    //   }
+    //   return Restaurants.create(restaurant);
+    // })
+    .then(() => {
+      console.log("done.");
+      process.exit();
+    });
+});
